755d95dc18724b920cc6fdd279aac411f50945fb
Abulgymbr0 <145333500+Abulgymbr0@users.noreply.github.com>
2025-03-07 08:25:34 +0000

    finihed

--- src/programs/c_program_00/main.c
+++ src/programs/c_program_00/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <inttypes.h>
 
-int main(int argc, char **argv) {
-  printf("This program was called with %d arguments.\n", argc);
-  printf("Hello World!\n");
+int main()
+{
+  int32_t arr[5];
+  printf("Hi lets sort numbers!\n");
+
+  for (int32_t i = 0; i < 5; i++)
+  {
+    printf("Please enter a for number %d: ", i + 1);
+    scanf("%d", &arr[i]);
+  }
+
+  for (int32_t i = 0; i < 5; i++)
+  {
+    printf("%d\n", arr[i]);
+  }
+
+  printf("Here are the sorted numbers:\n");
+  for (int32_t i = 0; i < 5; i++)
+  {
+    for (int32_t i2 = 0; i2 < 5-1; i2++)
+    {
+      if (arr[i2] > arr[i2 + 1])
+      {
+        int32_t tmp = arr[i2];
+        arr[i2] = arr[i2 + 1];
+        arr[i2 + 1] = tmp;
+      }
+    }
+  }
+
+  for (int32_t i = 0; i < 5; i++)
+  {
+    printf("%d\n", arr[i]);
+  }
 }
